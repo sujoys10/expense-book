85ec24c133437e7fc291ce378be2a72f289cb908
Sujoy Saha <sujoys023@gmail.com>
2020-06-18 00:31:51 +0530

    env var

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,7 +3,7 @@ import 'firebase/database';
 import 'firebase/auth';
 
 const config = {
-    apiKey: process.env.GOOGLE_API_KEY,
+    apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
     authDomain: "expense-book-14efe.firebaseapp.com",
     databaseURL: "https://expense-book-14efe.firebaseio.com/",
     storageBucket: "bucket.appspot.com"
@@ -16,22 +16,3 @@ var googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 const database = firebase.database();
 
 export { firebase, auth, googleAuthProvider, database as default};
-/* const userId =1;
-const month = 'JAN'
-const item = database.ref('users/' + userId + '/templates' + month).push().key;
-console.log(item)
-database.ref('users/' + userId + '/templates' + month)
-    .push({
-        title: 'bus',
-        catagory: 'transport',
-        defaultExpense: '20'
-    })
-
-var connectedRef = database.ref(".info/connected");
-connectedRef.on("value", function(snap) {
-    if (snap.val() === true) {
-    alert("connected");
-    } else {
-    alert("not connected");
-    }
-});     */
